05cfe013183008399a88519ebefd8df04f404642
Muhamad Sidik Sugiri <sugiri2020@gmail.com>
2020-12-27 18:23:42 +0700

    upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta href="utf-8">
+	<title>UNSIKA WEBPAGE</title>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,900i&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="css/style.css">
+	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row backgroundblue">
+			<div class="col-md-2">
+				<img class="img-responsive" src="images/logo.jpg" alt="logo unsika">
+			</div>
+			<div class="col-md-8">
+				<h1 class="text-center marginatas">Universitas Singaperbangsa Karawang</h1>
+				<hr>
+				<h3 class="text-thin text-center">Penerimaan Mahasiswa Baru</h3>
+			</div>
+			<div class="col-md-2">
+				<img class="img-responsive" src="images/kemdikbud.png" alt="logo kemdikbud">
+				<form class="form1">
+					<input class="search1" type="text" placeholder="">
+					<input class="button2" type="button" value="Cari">
+				</form>
+			</div>
+		</div>
+
+		<div class="row warnaabuabu">
+		<nav class="navbar navbar-default">
+	    <div class="col-md-12">
+	
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="">Home</a></li>
+					<li><a href="#">Profil</a></li>
+					<li><a href="#">Tentang Kami</a></li> 
+					<li><a href="#">Kontak</a></li> 
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">Tutorial
+						<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="www.facebook.com">HTML</a></li>
+							<li><a href="www.twitter.com">CSS</a></li>
+							<li><a href="www.instagram.com">Bootstrap</a></li> 
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li><a href="#"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+		    </div>
+		</nav>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<img class="img-responsive" src="https://placekitten.com/1100/400" alt="gambar utama">
+			</div>
+		</div>
+		<div class="row backgroundblue">
+			<div class="col-md-2">
+				<img class="img-responsive"src="images/informatika.png" alt="img informatika">
+			</div>
+			<div class="col-md-5">
+				<div class="tengah">
+				<h4>Alamat</h4>
+				<h5>Jl. H.S Ronggowaluyo Telukjambe Karawang 41361</h5>
+				<h5>Telepon: (0267) 641177, 641367, 642582</h5>
+				<h5>Fax: (0267) 641177, 641367, 642582</h5>
+				</div>
+			</div>
+			<div class="col-md-5">
+				<div>
+				<a href="https://www.facebook.com/fasilkom.unsika" target="_blank">
+					<img class="pull-right img-responsive" src="https://placekitten.com/100" alt="logo medsos">
+				</a>
+
+				<a href="https://www.twitter.com/unsika" target="_blank">
+					<img class="pull-right img-responsive"src="https://placekitten.com/100" alt="logo twitter">
+				</a>
+				</div>
+				<div class="">
+				<a href="https://www.unsika.ac.id">
+					<h5>Website:http://www.unsika.ac.id</h5>
+				</a>
+				<h5>E-mail: info@unsika.ac.id</h5>
+				<h5>Rektor: Proff. Dr. Hj, Moh. Wahyudin Zarkasyu, SE. MS. Ali. CPA</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
